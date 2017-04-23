cc7e110ed8e7170d6af34065557224e4fb904224
Artem Kostenko <kosten27@gmail.com>
2017-04-23 09:42:45 +0300

    /

--- app/src/main/java/ua/sten/messenger/CreateMessageActivity.java
+++ app/src/main/java/ua/sten/messenger/CreateMessageActivity.java
@@ -1,9 +1,13 @@
 package ua.sten.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -12,11 +16,15 @@ public class CreateMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
 
+        final EditText message = (EditText) findViewById(R.id.edit_message);
+
         Button sendMessage = (Button) findViewById(R.id.send);
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(getApplicationContext(), ReceiveMessageActivity.class);
+                intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, message.getText().toString());
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/ua/sten/messenger/ReceiveMessageActivity.java
+++ app/src/main/java/ua/sten/messenger/ReceiveMessageActivity.java
@@ -2,12 +2,18 @@ package ua.sten.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        TextView message = (TextView) findViewById(R.id.message);
+        message.setText(getIntent().getStringExtra(EXTRA_MESSAGE));
     }
 }
